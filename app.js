db5ea605e94362834cef5dd17711813705cd4e50
SSJay5 <48097747+SSJay5@users.noreply.github.com>
2020-11-12 01:35:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,80 @@
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const emergencyRouter = require('./routes/emergencyRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
+//Start Expess APP
 const app = express();
+app.set('port', process.env.PORT || 3000);
 
-const x = 100;
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Set security HTTP headers
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+app.use('/', viewRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/emergencies', emergencyRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!!`, 404));
+});
+app.use(globalErrorHandler);
+
+module.exports = app;
